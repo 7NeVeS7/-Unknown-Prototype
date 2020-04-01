0c3e01b4f96b38c576d858d0a2ecbba731302afc
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-04-01 21:03:10 +0200

    Movement

---
+++ -Unknown-Prototype/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    [SerializeField]
+    private Rigidbody2D rb;
+    private Vector2 _movement;
+    private string _direction = "Vertical";
+    private void Update()
+    {
+        _movement.y = Input.GetAxisRaw(_direction);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + _movement * _moveSpeed * Time.fixedDeltaTime);
+    }
+}
