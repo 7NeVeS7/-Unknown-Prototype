02c30f616c8d635931c06075f570dba7c53e2e5d
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-04-23 17:09:27 +0200

    RandomColorsSpawn

--- -Unknown-Prototype/Assets/Scripts/deployColors.cs
+++ -Unknown-Prototype/Assets/Scripts/deployColors.cs
@@ -1,26 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class deployColors : MonoBehaviour
 {
     [SerializeField]
     private GameObject _redPrefab;
     [SerializeField]
     private float _respawnTime = 1f;
-    private Vector2 screenBounds;
+    private Vector2 _screenBounds;
     void Start()
     {
-        
+        _screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        StartCoroutine(ColorWave());
     }
     private void _SpawnEnemy()
     {
         GameObject a = Instantiate(_redPrefab) as GameObject;
-        //a.transform.position = new Vector2(screenBounds.x*-2, Random.Range(-screenBounds.y, screenBounds.y));
+        a.transform.position = new Vector2(_screenBounds.x*2, UnityEngine.Random.Range(-_screenBounds.y, _screenBounds.y));
     }
-    private void Update()
+    IEnumerator ColorWave()
     {
-         
+        while(true)
+        {
+            yield return new WaitForSeconds(_respawnTime);
+            _SpawnEnemy();
+        }
     }
 
 }
