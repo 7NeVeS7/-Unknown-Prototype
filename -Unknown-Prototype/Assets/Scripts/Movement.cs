0a6424247e437599a2133d3feb7ef07719dfa186
jahoo99 <62152889+jahoo99@users.noreply.github.com>
2020-05-14 19:57:02 +0200

    Joystick

--- -Unknown-Prototype/Assets/Scripts/Movement.cs
+++ -Unknown-Prototype/Assets/Scripts/Movement.cs
@@ -4,20 +4,55 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField]
-    private float _moveSpeed = 5f;
-    [SerializeField]
-    private Rigidbody2D _rb;
+    private float _moveHorizontal;
+    private float _moveVertical;
     private Vector2 _movement;
-    private string _directionY = "Vertical";
-   
-    private void Update()
+    private Rigidbody2D _rb2D;
+    [SerializeField]
+    private float _speed = 5f;
+    [SerializeField]
+    private Joystick _joystick;
+
+    private void Start()
     {
-        _movement.y = Input.GetAxisRaw(_directionY);
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            _joystick.gameObject.SetActive(true);
+            Screen.orientation = ScreenOrientation.LandscapeLeft;
+            Application.targetFrameRate = 60;
+        }
+    }
+    void Awake()
+    {
+        _rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        _moveHorizontal = _joystick.Horizontal;
+        _moveVertical = _joystick.Vertical;
+
+        _movement = new Vector2(_moveHorizontal, _moveVertical);
+        _movement = _movement.normalized;
+        _rb2D.AddForce(_movement * _speed * Time.fixedDeltaTime,  ForceMode2D.Impulse);
+
     }
 
-    private void FixedUpdate()
-    {
-        _rb.MovePosition(_rb.position + _movement * _moveSpeed * Time.fixedDeltaTime);
-    }
+
+
+    //[SerializeField]
+    //private float _moveSpeed = 5f;
+    //[SerializeField]
+    //private Rigidbody2D _rb;
+    //private Vector2 _movement;
+    //private string _directionY = "Vertical";
+
+    //private void Update()
+    //{
+    //    _movement.y = Input.GetAxisRaw(_directionY);
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    _rb.MovePosition(_rb.position + _movement * _moveSpeed * Time.fixedDeltaTime);
+    //}
 }
